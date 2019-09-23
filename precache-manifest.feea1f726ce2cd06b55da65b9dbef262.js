5390fcee1aa20bd818c98a06ce39ab5c8ef607f3
Josue Valencia <josue.valencia@domo.ec>
2019-09-23 20:46:03 +0200

    Updates

---
+++ precache-manifest.feea1f726ce2cd06b55da65b9dbef262.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575fa01491e9c701fb2bb60b2e2693c8",
+    "url": "https://josuevalrob.github.io/jeval-web/index.html"
+  },
+  {
+    "revision": "79d8b6ad75a951ec451b",
+    "url": "https://josuevalrob.github.io/jeval-web/static/js/2.9a48f43f.chunk.js"
+  },
+  {
+    "revision": "bd197be91bfa0b5d8ca2",
+    "url": "https://josuevalrob.github.io/jeval-web/static/js/main.e7754a47.chunk.js"
+  },
+  {
+    "revision": "8bde37af4e1d58c0dda4",
+    "url": "https://josuevalrob.github.io/jeval-web/static/js/runtime~main.7c8c6f62.js"
+  }
+]);
