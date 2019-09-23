9e1e3f64d4ea0a6032980bfbd6a87f081540a29f
Josue Valencia <josue.valencia@domo.ec>
2019-09-23 20:30:04 +0200

    Updates

---
+++ precache-manifest.b3dcf4bd2a22eabd9f380c25381c976e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96536c6095a3953bdbdae742f2b91c9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79d8b6ad75a951ec451b",
+    "url": "/static/js/2.9a48f43f.chunk.js"
+  },
+  {
+    "revision": "0e044a23654aa3a2aad2",
+    "url": "/static/js/main.5813c120.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "jeval-web/precache-manifest.873a6b1e308e0e3203598b1a4b72582a.js"
+  "/precache-manifest.b3dcf4bd2a22eabd9f380c25381c976e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("jeval-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
