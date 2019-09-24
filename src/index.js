3bec98425dc7e04d8531dc9018347410a099bd7e
Josue Valencia <josue.valencia@domo.ec>
2019-09-24 19:22:44 +0200

    empty .env file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthStore } from './contexts/AuthStore';
-
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
     <AuthStore>
