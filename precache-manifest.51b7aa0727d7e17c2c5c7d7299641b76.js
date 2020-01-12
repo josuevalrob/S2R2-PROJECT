b2bc789938f4b908249d9c5ca18a3ce440d33fa8
josuevalrob <josue.valrob@gmail.com>
2020-01-12 15:58:51 +0000

    Deploying to master from  e5687a74b4592b0fbb408c807e33e7c94b4e46da

---
+++ precache-manifest.51b7aa0727d7e17c2c5c7d7299641b76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73df1718eb2802329ba2d7b8fed41e54",
+    "url": "/jeval-web/index.html"
+  },
+  {
+    "revision": "2f32de030e3ed715edb1",
+    "url": "/jeval-web/static/css/main.9411e819.chunk.css"
+  },
+  {
+    "revision": "8437cb2e8639cb6b89c3",
+    "url": "/jeval-web/static/js/2.d899bcfa.chunk.js"
+  },
+  {
+    "revision": "3426ef915159ac0a641e6f4b208be21b",
+    "url": "/jeval-web/static/js/2.d899bcfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f32de030e3ed715edb1",
+    "url": "/jeval-web/static/js/main.6f6428de.chunk.js"
+  },
+  {
+    "revision": "699e446c0a82dbf4ae43",
+    "url": "/jeval-web/static/js/runtime-main.4007c2e4.js"
+  }
+]);
